2feae5f03ef529137d4a82cc6745d939c3a4ad51
A-MRizk <rizkanthony176@gmail.com>
2024-12-04 18:35:38 +0200

    .

--- components/ProductThumb.tsx
+++ components/ProductThumb.tsx
@@ -1,4 +1,6 @@
+import { imageUrl } from "@/lib/imageUrl";
 import { Products } from "@/sanity.types";
+import Image from "next/image";
 import Link from "next/link";
 
 function ProductThumb({ product }: { product: Products }) {
@@ -6,11 +8,11 @@ function ProductThumb({ product }: { product: Products }) {
 
   return (
     <Link
-      href={`/studio/structure/product/${product.slug?.current}`}
+      href={`/product/${product.slug?.current}`}
       className={`group flex flex-col bg-white rounded-lg border border-gray-200 shadow-sm hover:shadow-md transition-all duration-200 overflow-hidden ${isOutOfStock ? "opacity-50" : ""}`}
     >
       <div className="relative aspect-square w-full h-full overflow-hidden">
-        {/* {product.image && (
+        {product.image && (
           <Image
             className="object-contain transition-transform duration-300 group-hover:scale-105"
             src={imageUrl(product.image).url()}
@@ -18,7 +20,7 @@ function ProductThumb({ product }: { product: Products }) {
             fill
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
           />
-        )} */}
+        )}
 
         {isOutOfStock && (
           <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50">
