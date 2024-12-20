1493c68999068004ed8551f843f26c0192f9f039
A-MRizk <rizkanthony176@gmail.com>
2024-12-20 18:49:26 +0200

    3rd commit

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -9,7 +9,6 @@ export default async function Home() {
 
   return (
     <div>
-      <h1>hello world</h1>
       <BlackFridayBanner />
       {/* render products */}
       <div className="flex flex-col items-center min-h-screen p-4 bg-gray-100">
